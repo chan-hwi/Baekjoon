bdbe9940ee54b7453c5a3cf57c8dd3fe5ad55fcd
chan-hwi <32610225+chan-hwi@users.noreply.github.com>
2022-08-29 21:32:16 +0900

    [Platinum V] Title: 두 가중치, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Platinum/12930. 두 가중치/두 가중치.cc
+++ 백준/Platinum/12930. 두 가중치/두 가중치.cc
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 #define INF 987654321
 using namespace std;
 
-struct Node { int i, bm, w[2]; };
+struct Node { int i, w1, w2; };
+
+struct cmp {
+    bool operator() (const Node &n1, const Node &n2) {
+        return n1.w2 > n2.w2;
+    }
+};
 
 int N, ans = INF;
-bool visit[20][1 << 20];
+int W[20][180];
 char edges[2][20][20];
-queue<Node> q;
+priority_queue<Node, vector<Node>, cmp> pq;
 
 int main() {
     cin.tie(NULL);  ios_base::sync_with_stdio(false);
@@ -21,21 +28,23 @@ int main() {
         }
     }
 
-    q.push({ 0, 1, { 0, 0 } });
-    visit[0][1] = true;
-    while (!q.empty()) {
-        Node n = q.front();
-        q.pop();
+    fill(&W[0][0], &W[19][180], 1000);
+    pq.push({ 0, 0, 0 });
+    W[0][0] = 0;
+    while (!pq.empty()) {
+        Node n = pq.top();
+        pq.pop();
 
+        if (W[n.i][n.w1] < n.w2) continue;
+        if (n.i == 1) {
+            ans = min(ans, n.w1 * n.w2);
+            continue;
+        }
         for (int j = 0; j < N; j++) {
-            Node nn = { j, n.bm | (1 << j), { n.w[0] + (edges[0][n.i][j] - '0'), n.w[1] + (edges[1][n.i][j] - '0') } };
-            if (edges[0][n.i][j] == '.' || visit[j][nn.bm]) continue;
-            if (j == 1) {
-                ans = min(ans, nn.w[0] * nn.w[1]);
-                continue;
-            }
-            q.push(nn);
-            visit[j][nn.bm] = true;
+            Node nn = { j, n.w1 + (edges[0][n.i][j] - '0'), n.w2 + (edges[1][n.i][j] - '0') };
+            if (edges[0][n.i][j] == '.' || nn.w1 >= 180 || W[j][nn.w1] <= nn.w2) continue;
+            W[j][nn.w1] = nn.w2;
+            pq.push(nn);
         }
     }
     cout << (ans == INF ? -1 : ans) << '\n';
